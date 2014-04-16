64b0abebf8fda6da88e943c26a735a63aaaaef7f
chm90 <christoffer.mockelind@live.se>
2014-04-16 17:05:08 +0200

    Remove abstract strategy

--- Strategy.java
+++ Strategy.java
@@ -17,5 +17,5 @@ public class Personality {
         this.foresightDistance = foresightDistance;
     }
 
-    public abstract double input(Vehicle mine, Vehicle next);
+    public double input(Vehicle mine, Vehicle next);
 }
