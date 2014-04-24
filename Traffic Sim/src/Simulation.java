421a9ae8714ae1436086d5a9617aa0c9000a0563
chm90 <christoffer.mockelind@live.se>
2014-04-24 13:54:27 +0200

    testtststtt

--- Traffic Sim/src/Simulation.java
+++ Traffic Sim/src/Simulation.java
@@ -17,6 +17,7 @@ public class Simulation {
     JPanel panel;
 	Graphics g;
 	ArrayList<Vehicle> vehicles;
+	int barfoobar;
 
     public static void main(String[] args) {
         Simulation s = new Simulation(100, 500);
